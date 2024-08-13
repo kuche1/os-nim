8526689a57a1786cf5ef1d5f68a4c75665ff7362
kuche1 <znamsiparolata4esno@abv.bg>
2024-08-13 17:01:38 +0300

    update

--- run.sh
+++ run.sh
@@ -32,3 +32,5 @@ qemu-system-x86_64 \
     -drive format=raw,file=fat:rw:diskimg \
     -machine q35 \
     -net none
+# `-machine q35` to use the Q35 + ICH9 chipsets (2009), instead of the default i440FX + PIIX chipsets (1996); this gives a more modern environment, with support for PCI Express, AHCI, and better UEFI, ACPI, and USB support
+# `-net none` to disable the default network card, to prevent the firmware from trying to use PXE network boot
