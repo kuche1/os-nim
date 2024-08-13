7142b50b6c476ad05794b14ebc3fa839c68c412c
kuche1 <znamsiparolata4esno@abv.bg>
2024-08-13 22:34:42 +0300

    update

--- run.sh
+++ run.sh
@@ -2,21 +2,6 @@
 
 set -euo pipefail
 
-# compile:
-# nim c --os:any src/main.nim
-#
-# verify:
-# file src/main
-# result:
-# src/main: PE32+ executable (EFI application) x86-64, for MS Windows, 4 sections
-#
-# compile:
-# nim c --os:any src/bootx64.nim
-# verify:
-# file src/bootx64
-# result:
-# src/bootx64: PE32+ executable (EFI application) x86-64, for MS Windows, 4 sections
-
 # https://0xc0ffee.netlify.app/osdev/01-intro.html
 
 nim c --os:any src/bootx64.nim
